3994deb2b2692889bd238ffb0ee585a61d2a432e
shushant kumar <rekhaubgb@gmail.com>
2023-07-19 18:56:07 +0530

    Create POTD 19JULY Longest Palindromic Subsequence.py

---
+++ POTD 19JULY Longest Palindromic Subsequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalinSubseq(self, S):
+        # code here
+        s1=S
+        s2=S[::-1]
+        n=len(S)
+        dp=[[0 for i in range(len(s1)+1)] for j in range(len(s1)+1)]
+        
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if(s1[i-1]==s2[j-1]):
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        ans=0
+        for i in dp:
+            for j in i:
+                ans=max(ans,j)
+        return ans
