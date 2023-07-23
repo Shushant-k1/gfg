4d21a6d206c39bebe17b58d6c96ffce26dabb27a
shushant kumar <rekhaubgb@gmail.com>
2023-07-23 20:21:42 +0530

    Create POTD 23 JULY Given a linked list of 0s, 1s and 2s, sort it.py

---
+++ POTD 23 JULY Given a linked list of 0s, 1s and 2s, sort it.py
@@ -0,0 +1,16 @@
+class Solution:
+    #Function to sort a linked list of 0s, 1s and 2s.
+    def segregate(self, head):
+        counts, node = [0] * 3, head
+        while node:
+            counts[node.data] += 1
+            node = node.next
+        node, i = head, 0
+        while node:
+            while counts[i] == 0:
+                i += 1
+            node.data = i
+            counts[i] -= 1
+            node = node.next
+        return head
+    
